9e3df43c82cbaaa24e9cf3427f8e70a3bdb5f386
Justin Fanton <justin.fanton@gmail.com>
2025-01-10 13:22:38 +0100

    resolved import errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/homepage/Homepage';
-import BookList from './components/BookList';
-import Profile from './components/Profile';
+import BookList from './components/bookList/BookList';
+import Profile from './components/profile/Profile';
 import About from './pages/about/About';
 
 function App() {
@@ -11,14 +11,12 @@ function App() {
       <div className="App">
         <nav>
           <Link to="/">Home</Link>
-          <Link to="/books">Book List</Link>
           <Link to="/profile">Profile</Link>
           <Link to="/about">Rules</Link>
         </nav>
 
         <Routes>
           <Route path="/" element={<Homepage />} />
-          <Route path="/books" element={<BookList />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/about" element={<About />} />
         </Routes>
