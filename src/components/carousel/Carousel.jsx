b0107188e752a2d96694353dd0c11c8f77bb67b4
Justin Fanton <justin.fanton@gmail.com>
2025-01-28 12:30:53 +0100

    Implemented routing to book details page

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -1,20 +1,47 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
+import { Link } from "react-router-dom";
 import "./carousel.css";
 
 const Carousel = ({ books }) => {
+  const carouselRef = useRef(null);
+
+  const handleMouseMove = (e) => {
+    const carousel = carouselRef.current;
+    if (!carousel) return;
+
+    const carouselWidth = carousel.offsetWidth;
+    const mouseX = e.clientX;
+    const scrollPosition = (mouseX / carouselWidth) * carousel.scrollWidth;
+
+    carousel.scrollLeft = scrollPosition;
+  };
+
+  useEffect(() => {
+    const carousel = carouselRef.current;
+    if (carousel) {
+      carousel.addEventListener("mousemove", handleMouseMove);
+    }
+
+    return () => {
+      if (carousel) {
+        carousel.removeEventListener("mousemove", handleMouseMove);
+      }
+    };
+  }, []);
+
   return books && books.length > 0 ? (
-    <div className="carousel">
+    <div className="carousel" ref={carouselRef}>
       <div className="carousel__list">
         {books.map((book, index) => (
           <div key={index} className="carousel__item">
-            <a href="#" className="carousel-item-link">
+            <Link to={`/book-details/${book._id}`} className="carousel-item-link">
               <img
                 src={book.bookImageUrl || "default-book.jpg"}
                 alt={book.title}
                 className="carousel-image"
               />
               <span className="carousel-item-title">{book.title}</span>
-            </a>
+            </Link>
           </div>
         ))}
       </div>
